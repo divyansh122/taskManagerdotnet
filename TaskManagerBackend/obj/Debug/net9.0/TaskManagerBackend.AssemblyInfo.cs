1d54d93a574d803e77b14f148419cf0f9f94063a
Divyansh Gupta <divyanshgupta335@gmail.com>
2025-04-03 20:46:36 +0530

    second commit

--- TaskManagerBackend/obj/Debug/net9.0/TaskManagerBackend.AssemblyInfo.cs
+++ TaskManagerBackend/obj/Debug/net9.0/TaskManagerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8827714be17f128a46ac6e3c1610d30823338289")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
